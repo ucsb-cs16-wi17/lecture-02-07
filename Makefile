ba7d64035285bd182ff9fe00257a441afa7f39e8
Diba Mirza <dimirza@cs.ucsb.edu>
2017-02-07 18:53:57 -0800

    Iterating in a loop to access array elements, basic makefiles

---
+++ Makefile
@@ -0,0 +1,3 @@
+#target
+score: scores.cpp
+	g++ -std=c++11 scores.cpp -o score
--- scores.cpp
+++ scores.cpp
@@ -9,14 +9,29 @@ using namespace std;
 
 
 int main(){
-    int score1, score2, score3, score4, score5;
-    score1 = 65;
-    score2 = 85;
-    score3 = 97;
-    score4 = 75;
-    score5 = 95;
-    double avg_score = (score1+score2+score3+score4+score5)/5;
+    int scores[5];
+    //Initialize scores
+    scores[0] = 65;
+    scores[1] = 85;
+    scores[2] = 97;
+    scores[3] = 75;
+    scores[4] = 95;
+    double sum=0.0;
+    double sum_c11=0.0;
+    for(int i=0;i<5;i++){
+        sum= sum+ scores[i];
+    }
+    
+    for(int score : scores){
+        sum_c11=sum_c11+score;
+    }
+
+    double avg_score = sum/5;
+    double avg_score_c11 = sum_c11/5;
+
     cout<<"\nAverage score is: "<<avg_score<<endl;
+    cout<<"\nAverage score in c++11 is: "<<avg_score_c11<<endl;
+
     return 0;
 }
 
